d0686777b2a32a9ceff32bc37c8c1990e9a96b11
Bohdan Marko <markopollo789@gmail.com>
2022-03-08 22:17:47 +0200

    code cleanup

--- LessonMonitor.WebAPI/Controllers/MemberController.cs
+++ LessonMonitor.WebAPI/Controllers/MemberController.cs
@@ -1,5 +1,6 @@
 ﻿using LessonMonitor.WebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace LessonMonitor.WebAPI.Controllers
 {
@@ -7,10 +8,14 @@ namespace LessonMonitor.WebAPI.Controllers
     [Route("[controller]")]
     public class MemberController : ControllerBase
     {
+        private readonly string[] _names = { "Bohdan", "Nazar", "Vasyl", "Roman" };
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(new Member { Age = 18, Name = "Bohdan"});
+            var rnd = new Random();
+
+            return Ok(new Member { Age = rnd.Next(10, 20), Name = _names[rnd.Next(0, _names.Length)]});
         }
     }
 }
