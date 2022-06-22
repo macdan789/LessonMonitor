285159d3816ee8c5b7736216e33a732345b2ed8d
Bohdan Marko <markopollo789@gmail.com>
2022-06-22 23:47:47 +0300

    DI first steps & README.md changes

---
+++ LessonMonitor.DI.Practice/Controller.cs
@@ -0,0 +1,55 @@
+﻿namespace LessonMonitor.DI.Practice
+{
+    class UserController : CommonClass
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _guid = Guid.NewGuid();
+            _userService = userService;
+        }
+
+        public override void GetGuid()
+        {
+            Console.WriteLine(nameof(UserController) + " | " + this._guid);
+            _userService.GetGuid();
+        }
+    }
+
+    class UserService : CommonClass
+    {
+        private readonly GithubClient _githubClient;
+
+        public UserService(GithubClient githubClient)
+        {
+            _guid = Guid.NewGuid();
+            _githubClient = githubClient;
+        }
+
+        public override void GetGuid()
+        {
+            Console.WriteLine(nameof(UserService) + " | " + this._guid);
+            _githubClient.GetGuid();
+        }
+    }
+
+    class GithubClient : CommonClass
+    {
+        public GithubClient()
+        {
+            _guid = Guid.NewGuid();
+        }
+
+        public override void GetGuid()
+            => Console.WriteLine(nameof(GithubClient) + " | " + this._guid);
+    }
+
+
+    public abstract class CommonClass
+    {
+        public Guid _guid;
+
+        public abstract void GetGuid();
+    }
+}
--- LessonMonitor.DI.Practice/Program.cs
+++ LessonMonitor.DI.Practice/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LessonMonitor.DI.Practice;
+using Microsoft.Extensions.DependencyInjection;
+
+//Custom view how lifetimes works (except Transient yet)
+SingletonView();
+ScopedView();
+
+//Add services using ServiceCollection
+var collection = new ServiceCollection();
+
+collection.AddTransient<GithubClient>();
+collection.AddTransient<UserService>();
+
+var provider = collection.BuildServiceProvider();
+
+using(var scope = provider.CreateScope())
+{
+    var service = scope.ServiceProvider.GetService<UserService>();
+    var controller = new UserController(service);
+    controller.GetGuid();
+}
+
+using (var scope = provider.CreateScope())
+{
+    var service = scope.ServiceProvider.GetService<UserService>();
+    var controller = new UserController(service);
+    controller.GetGuid();
+}
+
+
+
+void SingletonView()
+{
+    // more like Singleton behaviour (the same instances during program execution)
+
+    var client = new GithubClient();
+    var service = new UserService(client);
+
+    //first request
+    {
+        var controller = new UserController(service);
+        controller.GetGuid();
+    }
+
+    //second request
+    {
+        var controller = new UserController(service);
+        controller.GetGuid();
+    }
+
+    //RESULT: we are using the same service & client objects for all request
+    //objects lifetime: during whole program execution
+}
+
+void ScopedView()
+{
+    //first request
+    {
+        var client = new GithubClient();
+        var service = new UserService(client);
+        var controller = new UserController(service);
+        controller.GetGuid();
+    }
+
+    //second request
+    {
+        var client = new GithubClient();
+        var service = new UserService(client);
+        var controller = new UserController(service);
+        controller.GetGuid();
+    }
+
+    //RESULT: for each request we are using different service & client objects
+    //objects lifetime: in scope of one request
+}
