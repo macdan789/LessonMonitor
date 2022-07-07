2fa4e2905b7f7a38488cb654cee0fcea60d0de08
Bohdan Marko <markopollo789@gmail.com>
2022-07-07 23:28:31 +0300

    remove DataBase project

--- LessonMonitor.DataBase/Tables/Member.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Member]
-(
-	[MemberID] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [FirstName] NCHAR(20) NOT NULL, 
-    [LastName] NCHAR(20) NOT NULL, 
-    [Age] INT NOT NULL, 
-    [Group] NCHAR(10) NULL DEFAULT 'Empty'
-)
